7ffe44e062a2b3e71e58d492dd5c2902554f7268
JeahaOh <jeaha1217@gmail.com>
2018-09-04 18:23:17 +0900

    Using Class

--- harder/src/main/java/App.java
+++ harder/src/main/java/App.java
@@ -1,10 +1,36 @@
 import java.util.Scanner;
 
 public class App {
-    //  field 요소 배열
-    static String[] names = new String[100];
-    static String[] emails = new String[100];
-    static String[] passwords = new String[100];
+    //  여러 속성값을 관리하기 쉽도록 사용자 정의 데이터 타입을 만들어 사용.
+    static class Member{
+        protected String name;
+        protected String email;
+        protected String password;
+        
+        //  인스턴스의 메모리를 다루는 operator = getter/setter = accessor = property = message
+        
+        public String getName() {
+            return name;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public String getPassword() {
+            return password;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+    //  Member를 담는 배열.
+    static Member[] members = new Member[100];
+    
     
     static int index = 0;
     
@@ -12,16 +38,18 @@ public class App {
     
     static void inputMembers() {
         while(true) {
+            Member m = new Member();
+            
             System.out.print("Name : ");
-            names[index] = keyIn.nextLine();
+            m.setName(keyIn.nextLine());
             
             System.out.print("eMail : ");
-            emails[index] = keyIn.nextLine();
+            m.setEmail(keyIn.nextLine());
             
             System.out.print("Password : ");
-            passwords[index] = keyIn.nextLine();
+            m.setPassword(keyIn.nextLine());
             
-            index++;
+            members[index++] = m;
             
             System.out.print("\nContinue? ( Y/n )");
             String answer = keyIn.nextLine();
@@ -33,7 +61,7 @@ public class App {
     
     static void printMembers() {
         for(int i = 0; i < index; i++) {
-            System.out.printf("%s, %s, %s \n", names[i], emails[i], passwords[i]);                
+            System.out.printf("%s, %s, %s \n", members[i].getName(), members[i].getEmail(), members[i].getPassword());                
         }
     }   //  printMembers
     
