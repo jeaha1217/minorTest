ca1b9309d7613605995c2fdb5640a7f0fc49c9da
JeahaOh <jeaha1217@gmail.com>
2018-09-04 17:43:48 +0900

    반복문을 이용하여 반복 입력기능 추가

---
+++ harder/README.md
@@ -0,0 +1,4 @@
+## java110-project minor-division
+- 회원정보 입력 받아 출력하기
+- 작업
+-- App.java변경
--- harder/src/main/java/App.java
+++ harder/src/main/java/App.java
@@ -6,16 +6,24 @@ public class App {
         //  1) 키보드 입력을 처리할 객체 준비
         Scanner keyIn = new Scanner(System.in);
         
-        //  2) 사용자로부터 회원 정보 입력 받기
-        System.out.print("Name : ");
-        String name = keyIn.nextLine();
+        //  2) 반복문을 이용하여 사용자로부터 회원 정보 입력 받기
+        while(true) {
+            System.out.print("Name : ");
+            String name = keyIn.nextLine();
         
-        System.out.print("eMail : ");
-        String email = keyIn.nextLine();
+            System.out.print("eMail : ");
+            String email = keyIn.nextLine();
         
-        System.out.print("Password : ");
-        String password = keyIn.nextLine();
+            System.out.print("Password : ");
+            String password = keyIn.nextLine();
         
-        System.out.printf("%s, %s, %s", name, email, password);
+            System.out.printf("%s, %s, %s", name, email, password);
+        
+            System.out.print("Continue? ( Y/n )");
+            String answer = keyIn.nextLine();
+            if(answer.toLowerCase().equals("n")) {
+                break;
+            }
+        }
     }
 }
