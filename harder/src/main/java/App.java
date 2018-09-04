23fc3519ab793351f2fa3c14fe620d8e6a130f6d
JeahaOh <jeaha1217@gmail.com>
2018-09-04 18:02:40 +0900

    Split Codes by Methods

--- harder/src/main/java/App.java
+++ harder/src/main/java/App.java
@@ -1,38 +1,47 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        //  filed 요소 배열
-        String[] names = new String[100];
-        String[] emails = new String[100];
-        String[] passwords = new String[100];
-        
-        int index = 0;
-        
-        Scanner keyIn = new Scanner(System.in);
-        
+    //  field 요소 배열
+    static String[] names = new String[100];
+    static String[] emails = new String[100];
+    static String[] passwords = new String[100];
+    
+    static int index = 0;
+    
+    static Scanner keyIn = new Scanner(System.in);
+    
+    static void inputMembers() {
         while(true) {
             System.out.print("Name : ");
             names[index] = keyIn.nextLine();
-        
+            
             System.out.print("eMail : ");
             emails[index] = keyIn.nextLine();
-        
+            
             System.out.print("Password : ");
             passwords[index] = keyIn.nextLine();
-        
-        
-            System.out.print("Continue? ( Y/n )");
+            
+            index++;
+            
+            System.out.print("\nContinue? ( Y/n )");
             String answer = keyIn.nextLine();
             if(answer.toLowerCase().equals("n")) {
                 break;
             }
-            
-            for(int i = 0; i < index; i++) {
-                System.out.printf("%s, %s, %s", names[i], emails[i], passwords[i]);                
-            }
-            
-            keyIn.close();
         }
-    }
+    }   //  inputMembers()
+    
+    static void printMembers() {
+        for(int i = 0; i < index; i++) {
+            System.out.printf("%s, %s, %s \n", names[i], emails[i], passwords[i]);                
+        }
+    }   //  printMembers
+    
+    public static void main(String[] args) {
+        inputMembers();
+        
+        keyIn.close();
+        
+        printMembers();
+    }   //  main
 }
