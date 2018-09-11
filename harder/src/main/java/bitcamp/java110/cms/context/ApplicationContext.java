cf3e2cb16aeb5fcec2a014cfe6299a14aebc7477
JeahaOh <jeaha1217@gmail.com>
2018-09-11 23:49:29 +0900

    injectDependency() 별도 클래스로 분리

--- harder/src/main/java/bitcamp/java110/cms/context/ApplicationContext.java
+++ harder/src/main/java/bitcamp/java110/cms/context/ApplicationContext.java
@@ -127,40 +127,6 @@ public class ApplicationContext {
         }
     }
     
-    private void injectDependency() {
-        //  objPool에 보관된 객체 목록을 꺼낸다.
-        Collection<Object> objList = objPool.values();
-        
-        for(Object obj : objList) {
-        //  목록에서 객체를 꺼내 Annotation이 붙은 method를 찾는다.
-            Method[] methods = obj.getClass().getDeclaredMethods();
-
-            for (Method m : methods) {
-                //  Autowired 없다면,
-                if(!m.isAnnotationPresent(Autowired.class)) continue;
-//                System.out.println(m.getName());
-                //  setter 메소드를 호출하기 위해 parameter값을 준비한다.
-//                Class<?>[] pramTypes = m.getParameterTypes();
-                
-                //  getParameterTypes()
-                //  -> parameter값 한개만 추출.
-                
-                //  setter 메서드의 파라메터 타입을 알아낸다.
-                Class<?> paramType = m.getParameterTypes()[0];
-                
-                //  그 파라메터 타입과 일치하는 객체가 objPool에서 꺼낸다.
-                Object dependency = getBean(paramType);
-                
-                if(dependency == null) continue;
-                
-                System.out.printf("Call %s()\n", m.getName());
-                try {
-                m.invoke(obj, dependency);
-                }   catch(Exception e) {}
-            }
-        }
-    }
-    
     /*
     private void callBeanPostProcessor() {
         Collection<Object> objList = objPool.values();
