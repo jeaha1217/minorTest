a8d3b5efb3dd73b28a8ce3932d766b4063a64900
JeahaOh <jeaha1217@gmail.com>
2018-09-18 23:24:28 +0900

    관련 컨트롤러 통합

---
+++ harder/src/main/java/bitcamp/java110/cms/control/StudentController.java
@@ -0,0 +1,94 @@
+package bitcamp.java110.cms.control;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import bitcamp.java110.cms.annotation.RequestMapping;
+import bitcamp.java110.cms.dao.StudentDao;
+import bitcamp.java110.cms.domain.Student;
+import bitcamp.java110.cms.server.Request;
+import bitcamp.java110.cms.server.Response;
+
+@Component
+public class StudentController {
+    StudentDao studentDao;
+    
+    @Autowired
+    public void setStudentDao(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
+    
+    @RequestMapping("student/add")
+    public void add(Request request, Response response) {
+        while(true) {
+            Student s = new Student();
+            
+            s.setName(request.getParameter("name"));
+            s.setEmail(request.getParameter("email"));
+            s.setPassword(request.getParameter("password"));
+            s.setSchool(request.getParameter("school"));
+            s.setWorking(Boolean.parseBoolean(request.getParameter("work")));
+            s.setTel(request.getParameter("tel"));
+            
+            PrintWriter out = response.getWriter();
+            try {
+                if(studentDao.insert(s) == 1) {
+                    out.println("저장했습니다.");
+                }
+            }   catch (Exception e) {
+                out.println("이미 존재하는 이메일 입니다.");
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    @RequestMapping("student/delete")
+    public void delete(Request request, Response response) {
+        int no = Integer.parseInt(request.getParameter("no"));
+        PrintWriter out = response.getWriter();
+        
+        if(studentDao.delete(no) > 0) {
+            out.println("삭제되었습니다.");
+            return;
+        }
+        System.out.println("해당하는 번호가 없습니다.");
+    }
+    
+    @RequestMapping("student/detail")
+    public void detail(Request request, Response response) {
+        int no = Integer.parseInt(request.getParameter("no"));
+        
+        Student student = studentDao.findByNo(no);
+        PrintWriter out = response.getWriter();
+        if(student == null) {
+            out.println("해당하는 번호가 없습니다.");
+            return;
+        }
+        
+        out.printf("이름 : %s\n", student.getName());
+        out.printf("이메일 : %s\n", student.getEmail());
+        out.printf("암호 : %s\n", student.getPassword());
+        out.printf("최종학력 : %s\n", student.getSchool());
+        out.printf("전화 : %s\n", student.getTel());
+        out.printf("재직여부 : %b\n", student.isWorking());
+    }
+    
+    @RequestMapping("student/list")
+    public void list(Request request, Response response) {
+        PrintWriter out = response.getWriter();
+        List<Student> list = studentDao.findAll();
+
+        for(Student s : list) {
+            out.printf("%d : %s, %s, %s, %b, \n",
+                    s.getNo(),
+                    s.getName(),
+                    s.getEmail(),
+                    s.getSchool(),
+                    s.isWorking()
+                    );
+        }
+    }
+}
---
+++ harder/src/main/java/bitcamp/java110/cms/control/TeacherController.java
@@ -0,0 +1,98 @@
+package bitcamp.java110.cms.control;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import bitcamp.java110.cms.annotation.RequestMapping;
+import bitcamp.java110.cms.dao.TeacherDao;
+import bitcamp.java110.cms.domain.Teacher;
+import bitcamp.java110.cms.server.Request;
+import bitcamp.java110.cms.server.Response;
+
+@Component
+public class TeacherController {
+    TeacherDao teacherDao;
+
+    @Autowired
+    public void setTeacherDao(TeacherDao teacherDao) {
+        this.teacherDao = teacherDao;
+    }
+
+    @RequestMapping("teacher/add")
+    public void add(Request request, Response response) {
+        PrintWriter out = response.getWriter();
+        while(true) {
+            Teacher t = new Teacher();
+
+            t.setName(request.getParameter("name"));
+            t.setEmail(request.getParameter("email"));
+            t.setPassword(request.getParameter("password"));
+            t.setTel(request.getParameter("tel"));
+            t.setSubject(request.getParameter("subject"));
+
+            try {
+                t.setPay(Integer.parseInt(request.getParameter("pay")));
+            }   catch(Exception e) {
+                out.println("\n!!급여항목 입력 오류!!\n");
+                continue;
+            }
+
+            if(teacherDao.insert(t) == 1) {
+                out.println("저장했습니다.");
+            }   else {
+                out.println("이미 존재하는 이메일 입니다.");
+            }
+        }
+    }
+    
+    @RequestMapping("teacher/delete")
+    public void delete(Request request, Response response) {
+        int no = Integer.parseInt(request.getParameter("no"));
+        PrintWriter out = response.getWriter();
+        
+        if(teacherDao.delete(no) > 0) {
+            out.println("삭제되었습니다.");
+            return;
+        }
+        out.println("해당하는 번호가 없습니다.");
+    }
+    
+    @RequestMapping("teacher/detail")
+    public void detail(Request request, Response response) {
+        int no = Integer.parseInt(request.getParameter("no"));
+        
+        Teacher teacher = teacherDao.findByNo(no);
+        PrintWriter out = response.getWriter();
+        
+        if(teacher == null) {
+            out.println("해당하는 번호가 없습니다.");
+            return;
+        }
+        
+        out.printf("이름 : %s\n", teacher.getName());
+        out.printf("이메일 : %s\n", teacher.getEmail());
+        out.printf("암호 : %s\n", teacher.getPassword());
+        out.printf("전화 : %s\n", teacher.getTel());
+        out.printf("급여 : %d\n", teacher.getPay());
+        out.printf("과목 : %s\n", teacher.getSubject());
+    }
+    
+    @RequestMapping("teacher/list")
+    public void list(Request request, Response response) {
+        List<Teacher> list = teacherDao.findAll();
+        
+        PrintWriter out = response.getWriter();
+        for(Teacher t : list) {
+            out.printf("%d : %s, %s, %d, [%s] \n",
+                    t.getNo(),
+                    t.getName(),
+                    t.getEmail(),
+                    t.getPay(),
+                    t.getSubject()
+                    );
+        }
+    }
+}
